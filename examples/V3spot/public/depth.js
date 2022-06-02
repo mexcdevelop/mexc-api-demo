4a928ced908f2efc29ded834578ee26ac6df1fad
MXCDev <48047229+MXCDev@users.noreply.github.com>
2022-06-02 11:15:29 +0800

    Revert "Nodejs demo"

--- examples/V3spot/public/depth.js
+++
@@ -1,7 +0,0 @@
-
-const V3Spot = require('../../../src/V3spot')
-
-const client = new V3Spot()
-
-client.depth('btcusdt', { limit: 5 }).then(response => client.logger.log(response.data))
-  .catch(error => client.logger.error(error))
--- src/error/error.js
+++
@@ -1,8 +0,0 @@
-class Error {
-    constructor (message) {
-      this.message = message
-      this.name = 'Error'
-    }
-  }
-  
-  module.exports = Error
